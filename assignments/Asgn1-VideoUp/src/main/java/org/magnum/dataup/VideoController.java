240f7e165efc218aa6226867693a5c629adbf911
Gareth Sullivan <sullivan.gareth@gmail.com>
2014-08-04 16:11:46 +0100

    assignment 1 now passes

---
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/VideoController.java
@@ -0,0 +1,147 @@
+/*
+ * 
+ * Copyright 2014 Jules White
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.magnum.dataup;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.magnum.dataup.model.VideoStatus.VideoState;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class VideoController {
+	
+	/**
+	 * You will need to create one or more Spring controllers to fulfill the
+	 * requirements of the assignment. If you use this file, please rename it
+	 * to something other than "AnEmptyController"
+	 * 
+	 * 
+		 ________  ________  ________  ________          ___       ___  ___  ________  ___  __       
+		|\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \     
+		\ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_   
+		 \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
+		  \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
+		   \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
+		    \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
+                                                                                                                                                                                                                                                                        
+	 * 
+	 */
+
+	// Store the videos
+	private Map<Long,Video> videos = new HashMap<Long, Video>();
+		
+	// key into the videos Map
+	private static final AtomicLong currentId = new AtomicLong(0L);
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video v){
+		return save(v);
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.POST)
+	public @ResponseBody VideoStatus setVideoData( @PathVariable("id") long id, @RequestParam("data") MultipartFile videoData, HttpServletResponse response){
+		Video currVideo = videos.get(id);
+		if(currVideo == null)
+		{
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return null;
+		}
+		else
+		{
+			try {
+					VideoFileManager vfm = VideoFileManager.get();
+					vfm.saveVideoData(currVideo, videoData.getInputStream());
+					return new VideoStatus(VideoState.READY);
+				
+			} catch (IOException e) {
+				
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+				return null;
+			}
+		}
+		
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.GET)
+	public void getVideoData( @PathVariable("id") long id, HttpServletResponse response){
+		Video currVideo = videos.get(id);
+		if (currVideo != null)
+		{
+			try {
+				VideoFileManager.get().copyVideoData(currVideo, response.getOutputStream());
+			} catch (IOException e) {
+				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			}
+		}
+		else
+		{
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> getVideoList(){
+		return videos.values();
+	}
+	
+	private String getDataUrl(long videoId)
+	{
+		String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+		return url; 
+	}
+
+ 	private String getUrlBaseForLocalServer() {
+       HttpServletRequest request = 
+           ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+       String base = 
+          "http://"+request.getServerName() 
+          + ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
+       return base;
+    }
+
+ 	public Video save(Video entity) {
+		checkAndSetId(entity);
+		videos.put(entity.getId(), entity);
+		return entity;
+	}
+
+	private void checkAndSetId(Video entity) {
+		if(entity.getId() == 0){
+			entity.setId(currentId.incrementAndGet());
+			entity.setDataUrl(getDataUrl(entity.getId()));
+		}
+	}
+}
